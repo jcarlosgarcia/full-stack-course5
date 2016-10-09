141edfa9ee10dd7223e9495679ede3918ffb16a6
José Carlos García <jcarlosgarcia@hotmail.com>
2016-10-10 00:31:24 +0200

    updated path to templates

--- module4/js/categories.component.js
+++ module4/js/categories.component.js
@@ -3,7 +3,7 @@
 
   angular.module('MenuApp')
     .component('categories', {
-    templateUrl: 'full-stack-course5/module4/js/templates/categories-list.template.html',
+    templateUrl: 'js/templates/categories-list.template.html',
     bindings: {
       items: '<'
     }
--- module4/js/items.component.js
+++ module4/js/items.component.js
@@ -3,7 +3,7 @@
 
   angular.module('MenuApp')
   .component('items', {
-    templateUrl: 'full-stack-course5/module4/js/templates/items-list.template.html',
+    templateUrl: 'js/templates/items-list.template.html',
     bindings: {
       items: '<',
     },
